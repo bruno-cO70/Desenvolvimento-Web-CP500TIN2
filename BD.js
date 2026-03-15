a1886428d105ee814e4a9c9130f41a60aedc5e0e
recamOo <lopesrodrigo942@gmail.com>
2026-03-14 23:52:08 -0300

    Corrigido erros 1.4.2

--- BD.js
+++ BD.js
@@ -2,22 +2,24 @@
 const SUPABASE_URL = 'https://ezytybxzenfyrsecwpta.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV6eXR5Ynh6ZW5meXJzZWN3cHRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1NDA3ODksImV4cCI6MjA4OTExNjc4OX0.eOieZBtwJd8SyA1neBtYY5XlJL520-uqqSnriW4X9rY';
 
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 // 2. Função para Cadastrar um novo usuário no Banco
 async function cadastrar() {
     const emailInput = document.getElementById('register-email').value;
     const senhaInput = document.getElementById('register-senha').value;
 
-    const { data, error } = await supabase.auth.signUp({
+    const { data, error } = await supabaseClient.auth.signUp({
         email: emailInput,
         password: senhaInput,
     });
 
     if (error) {
-        document.getElementById('mensagem').innerText = "Erro ao cadastrar: " + error.message;
+        document.getElementById('mensagem-register').style.color = '#f87171';
+        document.getElementById('mensagem-register').innerText = "Erro ao cadastrar: " + error.message;
     } else {
-        document.getElementById('mensagem').innerText = "Cadastro realizado! Verifique seu e-mail para confirmar.";
+        document.getElementById('mensagem-register').style.color = '#4ade80';
+        document.getElementById('mensagem-register').innerText = "Cadastro realizado! Verifique seu e-mail para confirmar.";
     }
 }
 
@@ -26,15 +28,17 @@ async function entrar() {
     const emailInput = document.getElementById('login-email').value;
     const senhaInput = document.getElementById('login-senha').value;
 
-    const { data, error } = await supabase.auth.signInWithPassword({
+    const { data, error } = await supabaseClient.auth.signInWithPassword({
         email: emailInput,
         password: senhaInput,
     });
 
     if (error) {
-        document.getElementById('mensagem').innerText = "Erro ao entrar: Verifique e-mail e senha.";
+        document.getElementById('mensagem-login').style.color = '#f87171';
+        document.getElementById('mensagem-login').innerText = "Erro ao entrar: Verifique e-mail e senha.";
     } else {
-        document.getElementById('mensagem').innerText = "Login feito com sucesso! Bem-vindo.";
+        document.getElementById('mensagem-login').style.color = '#4ade80';
+        document.getElementById('mensagem-login').innerText = "Login feito com sucesso! Bem-vindo.";
         console.log("Dados do usuário logado:", data);
         
         // Redirecionar o usuário para a página de perfil
